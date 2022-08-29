1c9aae971f932076e311de8de9a0a8fb8cb5c452
BrenoRCBrito <brenorcbrito@gmail.com>
2022-08-28 21:35:31 -0300

    feat(update nest.js module): :sparkles: add new modules to app.module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { AuthModule } from './auth/auth.module';
+import { ClientModule } from './client/client.module';
+import { DoctorModule } from './doctor/doctor.module';
+import { GroupInviteModule } from './group-invite/group-invite.module';
+import { GroupModule } from './group/group.module';
+import { JobTypeModule } from './job-type/job-type.module';
+import { JobModule } from './job/job.module';
+import { PaymentModule } from './payment/payment.module';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { typeDefs as scalarTypeDefs } from 'graphql-scalars';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      typePaths: ['./**/*.graphql', ...scalarTypeDefs],
+    }),
+    AuthModule,
+    GroupModule,
+    DoctorModule,
+    GroupInviteModule,
+    ClientModule,
+    JobModule,
+    JobTypeModule,
+    PaymentModule,
+  ],
 })
 export class AppModule {}
