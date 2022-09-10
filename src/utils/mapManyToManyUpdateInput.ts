edb9006a58e2ec6e048ebddc5e59b1519357029e
BrenoRCBrito <brenorcbrito@gmail.com>
2022-09-10 19:27:11 -0300

    feat(util): :sparkles: add doctors to mapManyToMany util

--- src/utils/mapManyToManyUpdateInput.ts
+++ src/utils/mapManyToManyUpdateInput.ts
@@ -1,13 +1,18 @@
 type ManyToManyRelationSimpleObject = {
   groups?: number[];
   clients?: number[];
+  doctors?: number[];
 };
 
 export default function mapManyToManyUpdateInput(
   updateInputObject: ManyToManyRelationSimpleObject | unknown,
 ) {
   return Object.entries(updateInputObject).reduce((accumulator, current) => {
-    if (current[0] === 'groups' || current[0] === 'clients') {
+    if (
+      current[0] === 'groups' ||
+      current[0] === 'clients' ||
+      current[0] === 'doctors'
+    ) {
       accumulator[current[0]] = {
         connect: current[1].map((relationId) => {
           if (relationId !== 0) {
